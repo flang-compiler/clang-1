8afb75d2d06e770be9671e5de799e5aa3cc1eaed
Alok Kumar Sharma <AlokKumar.Sharma@amd.com>
2020-02-24 11:06:37 +0530

    Default dwarf version should be 4 for -g with flang
    Currently flang dumps dwarf version 2 for -g and 4 for absence of -g
    
    -------------------------
    $ flang my.f90
    $ llvm-dwarfdump a.out | grep version
    0x00000000: Compile Unit: length = 0x0000003d version = 0x0004 abbr_offset = 0x0000 addr_size = 0x08 (next unit at 0x00000041)
    
    $ flang -g my.f90
    $ llvm-dwarfdump a.out | grep version
    0x00000000: Compile Unit: length = 0x00000047 version = 0x0002 abbr_offset = 0x0000 addr_size = 0x08 (next unit at 0x0000004b)
    -------------------------
    
    It should be 4 for -g as it is the case with clang.
    
    Change-Id: Iae79dfeff75e372599fcb24507036c6d5c88ac59

--- lib/Driver/ToolChains/Flang.cpp
+++ lib/Driver/ToolChains/Flang.cpp
@@ -312,8 +312,8 @@ void FlangFrontend::ConstructJob(Compilation &C, const JobAction &JA,
     CommonCmdArgs.push_back("-x");
     CommonCmdArgs.push_back("120");
 
-    if (!GDwarfArg) // -g should produce default (DWARFv2)
-      CommonCmdArgs.push_back("0x200");
+    if (!GDwarfArg) // -g should produce default (DWARFv4)
+      CommonCmdArgs.push_back("0x1000000");
     else if (GDwarfArg->getOption().matches(options::OPT_gdwarf_2)) // -gdwarf-2
       CommonCmdArgs.push_back("0x200");
     else if (GDwarfArg->getOption().matches(options::OPT_gdwarf_3)) // -gdwarf-3
